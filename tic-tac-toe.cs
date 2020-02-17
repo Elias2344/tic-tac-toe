54517945d2da210fb8a261f133ace516c896abea
Elias2344 <eliasjamartinez@hotmail.com>
2020-02-17 09:42:10 -0700

    Add user input

--- tic-tac-toe.cs
+++ tic-tac-toe.cs
@@ -41,11 +41,55 @@ namespace Tic_Tac_Toe
           //Necesitamos el valor a agregar ("X", "O")
           matrix[y, x] = value;
         }
+
+        static void InputRequest() {
+          Console.WriteLine("Escribe las coordenadas de la forma y, x en donde quieres hacer tu movimiento y presione enter. ");
+          string userInputCoordinates = Console.ReadLine();
+
+          //Quitar espacios
+          userInputCoordinates = userInputCoordinates.Replace(" ", "");
+
+          //Separar en un arreglo de valores con ","
+          string[] coordinates = userInputCoordinates.Split(",");
+
+          //convertir en coordenadas tipo entero
+          int y = Convert.ToInt32(coordinates[0]);
+          int x = Convert.ToInt32(coordinates[1]);
+
+          AddValue('X', y, x);
+        }
         static void Main(string[] args)
         {
           PrintMatriz();
-          AddValue('X', 0, 0);
+          InputRequest();
           PrintMatriz();
+
+
+          bool gameEnded = false;
+          int turns = 0;
+
+          while (!gameEnded) {
+            InputRequest();
+            turns++;
+            //Check if user won
+            gameEnded = CheckThreeLines();
+
+            //End after 9 turns
+             if (turns >= 9) {
+              gameEnded = true;
+            } 
+
+            if (!gameEnded) {
+            //AIRequest();
+            turns++;
+            //Check if AI won
+              gameEnded = CheckThreeLines();
+            }
+
+           
+
+            gameEnded = CheckTreeLines() && IsMatrixFull();
+          }
         }
     }
 }
