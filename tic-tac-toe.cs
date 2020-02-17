5e34b29d32f17775883db54cbda63a0ba18d3ed0
Elias2344 <eliasjamartinez@hotmail.com>
2020-02-17 08:33:29 -0700

    Metodo añadido

--- tic-tac-toe.cs
+++ tic-tac-toe.cs
@@ -4,9 +4,19 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
+      /// <summary>
+      /// Matrix length
+      /// </summary>
         const int MATRIX_SIZE = 3;
         //y.x
+        /// <summary>
+        /// Inner matrix array [y, x]
+        /// </summary>
+        /// <value>Empty matrix</value>
           static  char[,] matrix = new char[MATRIX_SIZE, MATRIX_SIZE] {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+          /// <summary>
+          /// Prints matrix.
+          /// </summary>
         static void PrintMatriz() {
               for(int y = 0; y < MATRIX_SIZE; y++){
                   string line = "";
@@ -20,13 +30,28 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("------");
             }
         }
-        
+        /// <summary>
+        /// Adds a value to the matrix in the specified position
+        /// </summary>
+        /// <param name="value">value to add</param>
+        /// <param name="y">y position</param>
+        /// <param name="x">x position</param>
+        static void AddValue(char value, int y , int x) {
+          //Necesitamos y, x
+          //Necesitamos el valor a agregar ("X", "O")
+          matrix[y, x] = value
+        }
         static void Main(string[] args)
         {
+          PrintMatriz();
+          AddValue('X', 0, 0);
           PrintMatriz();
         }
     }
 }
     
+
+
+ 
 
 
